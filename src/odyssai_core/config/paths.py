2a4c60b154bc93b0bf12ea524ff95c70ffa7f24c
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-07-30 11:48:33 +0200

    Mettre à jour BASE_DIR pour inclure le répertoire parent et ajouter des configurations pour Google Cloud et LangSmith dans settings.py

--- src/odyssai_core/config/paths.py
+++ src/odyssai_core/config/paths.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
 SECRET_DIR = BASE_DIR / ".secrets"
 TEST_DIR = BASE_DIR / "tests"
--- src/odyssai_core/config/settings.py
+++ src/odyssai_core/config/settings.py
@@ -1,16 +1,23 @@
 import os
 from dotenv import load_dotenv
+from .paths import SECRET_DIR
 
 load_dotenv()
 
 # OpenAI
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
+# Google Cloud Text-to-Speech configuration
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(SECRET_DIR / "google_tts.json")
+
 # Hugging Face
 HF_API_KEY = os.getenv("HF_API_KEY")
 
 # Pinecone
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+
+# LangSmith
+LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
 
 # SerpAPI
 SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")

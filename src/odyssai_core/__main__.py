493f4ae9acf26d3fd73e4b6fcd7d1029c6882d82
Joachim Alexandre Jasmin <joachim.jasmin-dev@proton.me>
2025-07-31 21:00:02 +0200

    Supprimer la fonction de génération de monde et le module context_graph, simplifiant ainsi le flux principal de l'application.

--- src/odyssai_core/__main__.py
+++ src/odyssai_core/__main__.py
@@ -1,11 +1,9 @@
-import os
 from .workflows.ask_user_graph import ask_user_graph, QuestionState
 from .workflows.world_graph import world_creation_graph
 from .workflows.lore_graph import lore_graph
-from .workflows.context_graph import build_context_graph
 
 
-def generate_world():
+def main():
     # Ask user for inputs about the world
     result_1 = ask_user_graph.invoke({})
     user_answers: QuestionState = result_1["responses"]
@@ -23,30 +21,6 @@ def generate_world():
     result_3 = lore_graph.invoke({"world_id": result_2["world_id"]})
 
     print("Job done! Lore created.")
-
-
-def main():
-    print("Welcome to Odyssai Core v0.1.0 \n")
-
-    def get_context():
-        context_type = "lore"
-        os.environ["LANGCHAIN_PROJECT"] = f"odyssai-{context_type}-context"
-        graph = build_context_graph()
-
-        result = graph.invoke(
-            {
-                "world_id": "a137b189-2dd6-4e30-90b6-f272c1395f01",
-                "context_type": context_type,
-            }
-        )
-        print(f"Documents ({len(result['retrieved_documents'])}) : \n")
-        print(result["retrieved_documents"])
-        print("\n")
-        print("Combined résumé : \n")
-        print(result[f"{context_type}_context"])
-
-    # get_context()
-    generate_world()
 
 
 if __name__ == "__main__":
